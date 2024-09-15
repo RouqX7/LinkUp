a5e85d8ac5152c8e7bba72d46a97f46a26adad1e
RouqX7 <farouq2169@gmail.com>
2024-09-15 16:52:32 +0100

    Initial Commit: Implement User Authentication
    - Integrated Appwrite as the backend database for user management.
    - Utilized shadcn for building UI components.
    - Established robust authentication features.

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Route,Routes } from 'react-router-dom'
+import './global.css'
+import { Home } from './_root/pages'
+import SignInForm from './_auth/forms/SignInForm'
+import SignUpForm from './_auth/forms/SignUpForm'
+import RootLayout from './_root/RootLayout'
+import AuthLayout from './_auth/AuthLayout'
+import { Toaster } from "@/components/ui/toaster"
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* Public routes */}
+            <Route element= {<AuthLayout />}>
+            <Route path='/sign-in' element= {<SignInForm/>} />
+            <Route path='/sign-up' element= {<SignUpForm/>} />
+            </Route>
+
+       {/* Private Routes */}
+       <Route element={<RootLayout/>}>
+       <Route index element={<Home/>}/>
+       </Route>
+        </Routes>
+
+        <Toaster />
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,102 @@
+import { INewUser } from "@/types";
+import { ID, Query } from 'appwrite';
+import { account, appWriteConfig, avatars, databases } from "./config";
+
+export async function createUserAccount(user: INewUser) {
+    try {
+        // Check if there's already an active session
+        const existingSession = await account.getSession('current');
+        if (existingSession) {
+            console.log('Session already active:', existingSession);
+            return existingSession;
+        }
+
+        // If no active session, create a new user account
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        );
+        if (!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl,
+        });
+
+        return newUser;
+
+    } catch (error) {
+        console.log("Error creating user account:", error);
+        return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string,
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+}) {
+    try {
+        const newUser = await databases.createDocument(
+            appWriteConfig.databaseId,
+            appWriteConfig.userCollectionId,
+            ID.unique(),
+            user
+        );
+        return newUser;
+
+    } catch (error) {
+        console.log('Error saving user to database:', error);
+    }
+}
+
+export async function signInToAccount(user: { email: string; password: string; }) {
+    try {
+        // Check if there's already an active session
+        const existingSession = await account.getSession('current');
+
+        if (existingSession) {
+            console.log('Session already active:', existingSession);
+            return existingSession;
+        }
+
+    } catch (sessionError) {
+        // No active session, proceed to create one
+        console.log('No active session, creating a new one.');
+    }
+
+    try {
+        const session = await account.createEmailPasswordSession(user.email, user.password);
+        return session;
+    } catch (error) {
+        console.log('Error during sign-in:', error);
+    }
+}
+
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await account.get();
+
+        if (!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appWriteConfig.databaseId,
+            appWriteConfig.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        );
+
+        if (!currentUser) throw Error;
+        return currentUser.documents[0];
+    } catch (error) {
+        console.log('Error fetching current user:', error);
+    }
+}
